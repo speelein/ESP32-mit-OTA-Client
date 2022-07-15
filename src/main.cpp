50339083be85949361183bcc9a78b9a1748749a9
Gunter Pruefer <pruefer.g@gmx.de>
2022-07-15 07:44:31 +0200

    SCR kommentiert

--- src/main.cpp
+++ src/main.cpp
@@ -206,7 +206,7 @@ void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
 }
 
-// Fuer SD-CardReader
+// Fuer SD-CardReader funktioniert nicht
 /*
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
   Serial.printf("Listing directory: %s\n", dirname);
