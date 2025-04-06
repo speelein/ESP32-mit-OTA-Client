0af464f33d7f4d9fe42922a404c748911a8d9c01
speelein <pruefer.g@gmx.de>
2025-04-06 08:26:27 +0200

    lcd print

--- src/main.cpp
+++ src/main.cpp
@@ -418,11 +418,11 @@ void loop()
   int isDST = tstruct.tm_isdst; // Check if DST is active
 
 if (isDST == 1) {
-    Serial.println("Daylight saving time is active.");
+    lcd.println("Sommertime is active.");
 } else if (isDST == 0) {
-    Serial.println("Standard time (wintertime) is active.");
+    lcd.println("Standard time (wintertime) is active.");
 } else {
-    Serial.println("DST information is not available.");
+    lcd.println("DST information is not available.");
 }
   
   // fuer Darstellung auf Display
